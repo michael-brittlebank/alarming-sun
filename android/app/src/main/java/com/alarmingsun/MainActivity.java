8844441917d75169b2fd6cbece917cc52bf19016
mike-stumpf <mike.a.stumpf@gmail.com>
2018-07-23 20:12:10 -0500

    keep device awake in android when app is active

--- android/app/src/main/java/com/alarmingsun/MainActivity.java
+++ android/app/src/main/java/com/alarmingsun/MainActivity.java
@@ -1,8 +1,22 @@
 package com.alarmingsun;
 
 import com.facebook.react.ReactActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
 
 public class MainActivity extends ReactActivity {
+
+    /**
+     * Keep screen awake as long as app is running
+     * https://thecodebarbarian.com/react-native-keep-awake-android-java
+     * @param savedInstanceState
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.getWindow()
+                .addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+    }
 
     /**
      * Returns the name of the main component registered from JavaScript.
